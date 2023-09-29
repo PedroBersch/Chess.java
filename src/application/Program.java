bf18f2d2dbd6e1a43e5767d3cae3f90c78776e06
Pedro Bersch <pedroleottebersch@gmail.com>
2023-09-29 09:12:49 -0300

    Start implement Board and Pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,10 @@
 package application;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class Program {
     public static void main(String[] args) {
-        Position po = new Position(1,8);
-        System.out.println(po);
+        
     }
 }
