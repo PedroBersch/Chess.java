01cd89f41ebc0b421acffff3d80c5147c2307e62
Pedro Bersch <pedroleottebersch@gmail.com>
2023-09-29 13:15:35 -0300

    BoardException and defensive programming

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -13,16 +13,16 @@ public class ChessMatch {
     }
     //Get matrix of pieces
     public ChessPiece[][] getPieces(){
-        ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumn()];
+        ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
         for (int i = 0 ; i < board.getRows(); i++) {
-            for (int j = 0; j < board.getColumn(); j++){
+            for (int j = 0; j < board.getColumns(); j++){
                 mat[i][j] = (ChessPiece) board.piece(i,j);
             }
         }
         return mat;
     }
     private void initialSetup(){
-        board.placePiece(new Rook(board, Color.WHITE), new Position(2,1));
-        board.placePiece(new King(board, Color.BLACK), new Position(3,2));
+        board.placePiece(new Rook(board, Color.WHITE), new Position(7,1));
+        board.placePiece(new King(board, Color.BLACK), new Position(6,1));
     }
 }
